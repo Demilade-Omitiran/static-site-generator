db6e00e6a2b9fe84b462833df26c1a58761c3a0f
Demilade Omitiran <omitirand@gmail.com>
2024-04-23 19:51:39 +0100

    Create HtmlNode class

---
+++ src/htmlnode.py
@@ -0,0 +1,29 @@
+class HtmlNode:
+  def __init__(self, tag=None, value=None, children=None, props=None):
+    self.tag = tag
+    self.value = value
+    self.children = children
+    self.props = props
+
+  def to_html(self):
+    raise NotImplementedError()
+  
+  def props_to_html(self):
+    html_props = []
+    for key in self.props:
+      prop_string = f"{key}="
+      if type(self.props[key]) == str:
+        prop_string += f"\"{self.props[key]}\""
+      else:
+        prop_string += f"{self.props[key]}"
+      html_props.append(prop_string)
+    
+    return " " + " ".join(html_props)
+  
+  def __repr__(self):
+    return f"""
+      tag: {self.tag},
+      value: {self.value},
+      children: {self.children},
+      props: {self.props}
+    """
