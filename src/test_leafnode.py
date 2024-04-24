cba6807e4862e7e6342e6846dfaed7a81dc4ca98
Demilade Omitiran <omitirand@gmail.com>
2024-04-24 10:58:46 +0100

    Add test for value error

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -12,9 +12,14 @@ class TestLeafNode(unittest.TestCase):
 
   def test_to_html(self):
     node = LeafNode("a", "link", { "href": "www.abc.com", "width": 20 })
-    node_to_html = node.to_html()
     self.assertEqual(node.to_html(), "<a href=\"www.abc.com\" width=20>link</a>")
 
+  def test_value_err(self):
+    with self.assertRaises(ValueError) as err:
+      node = LeafNode("a", None, { "href": "www.abc.com", "width": 20 })
+      node_to_html = node.to_html()
+      self.assertEqual(err.msg, "value is required")
+    
 
 if __name__ == "__main__":
   unittest.main()
