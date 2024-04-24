d11e8a0d51faf28fee099ab87a5ea32fe9d87df7
Demilade Omitiran <omitirand@gmail.com>
2024-04-24 12:07:15 +0100

    Update text_types

--- src/main.py
+++ src/main.py
@@ -3,19 +3,20 @@ from leafnode import LeafNode
 
 def text_node_to_html_node(text_node):
   converter_function_dict = {
-    "text_type_text": lambda node: LeafNode(None, node.text),
-    "text_type_bold": lambda node: LeafNode("b", node.text),
-    "text_type_italic": lambda node: LeafNode("i", node.text),
-    "text_type_code": lambda node: LeafNode("code", node.text),
-    "text_type_link": lambda node: LeafNode("a", node.text, { "href": node.url }),
-    "text_type_image": lambda node: LeafNode("img", None, { "src": node.url, "alt": node.text })
+    "text": lambda node: LeafNode(None, node.text),
+    "bold": lambda node: LeafNode("b", node.text),
+    "italic": lambda node: LeafNode("i", node.text),
+    "code": lambda node: LeafNode("code", node.text),
+    "link": lambda node: LeafNode("a", node.text, { "href": node.url }),
+    "image": lambda node: LeafNode("img", None, { "src": node.url, "alt": node.text })
   }
-  
   if text_node.text_type not in converter_function_dict:
-    raise Exception("Invalid text_type")
-  
+    raise Exception("Invalid text_type") 
   converter_function = converter_function_dict[text_node.text_type]
   return converter_function(text_node)
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+  pass
 
 def main():
   node = TextNode("This is a text node", "bold", "https://www.boot.dev")
--- src/test_text_node_to_html_node_converter.py
+++ src/test_text_node_to_html_node_converter.py
@@ -6,7 +6,7 @@ from main import text_node_to_html_node
 
 class TestLeafNode(unittest.TestCase):
   def test_text_type_text(self):
-    node = TextNode("This is a text node", "text_type_text", "www.abc.com")
+    node = TextNode("This is a text node", "text", "www.abc.com")
     leaf_node = text_node_to_html_node(node)
     self.assertEqual(leaf_node.tag, None)
     self.assertEqual(leaf_node.value, "This is a text node")
@@ -14,7 +14,7 @@ class TestLeafNode(unittest.TestCase):
     self.assertEqual(leaf_node.props, None)
 
   def test_text_type_bold(self):
-    node = TextNode("This is a text node", "text_type_bold", "www.abc.com")
+    node = TextNode("This is a text node", "bold", "www.abc.com")
     leaf_node = text_node_to_html_node(node)
     self.assertEqual(leaf_node.tag, 'b')
     self.assertEqual(leaf_node.value, "This is a text node")
@@ -22,7 +22,7 @@ class TestLeafNode(unittest.TestCase):
     self.assertEqual(leaf_node.props, None)
 
   def test_text_type_italic(self):
-    node = TextNode("This is a text node", "text_type_italic", "www.abc.com")
+    node = TextNode("This is a text node", "italic", "www.abc.com")
     leaf_node = text_node_to_html_node(node)
     self.assertEqual(leaf_node.tag, 'i')
     self.assertEqual(leaf_node.value, "This is a text node")
@@ -30,7 +30,7 @@ class TestLeafNode(unittest.TestCase):
     self.assertEqual(leaf_node.props, None)
 
   def test_text_type_code(self):
-    node = TextNode("This is a text node", "text_type_code", "www.abc.com")
+    node = TextNode("This is a text node", "code", "www.abc.com")
     leaf_node = text_node_to_html_node(node)
     self.assertEqual(leaf_node.tag, 'code')
     self.assertEqual(leaf_node.value, "This is a text node")
@@ -38,7 +38,7 @@ class TestLeafNode(unittest.TestCase):
     self.assertEqual(leaf_node.props, None)
 
   def test_text_type_link(self):
-    node = TextNode("This is a text node", "text_type_link", "www.abc.com")
+    node = TextNode("This is a text node", "link", "www.abc.com")
     leaf_node = text_node_to_html_node(node)
     self.assertEqual(leaf_node.tag, 'a')
     self.assertEqual(leaf_node.value, "This is a text node")
@@ -46,7 +46,7 @@ class TestLeafNode(unittest.TestCase):
     self.assertEqual(leaf_node.props, { "href": "www.abc.com" })
 
   def test_text_type_image(self):
-    node = TextNode("This is a text node", "text_type_image", "www.abc.com")
+    node = TextNode("This is a text node", "image", "www.abc.com")
     leaf_node = text_node_to_html_node(node)
     self.assertEqual(leaf_node.tag, 'img')
     self.assertEqual(leaf_node.value, None)
