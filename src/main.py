3f4c759c5d86094760583bffa0fae485fe17c83d
Demilade Omitiran <omitirand@gmail.com>
2024-04-26 17:32:14 +0100

    Import and call generate_page function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-from textnode import TextNode
 from copy_static_to_public import copy_static_to_public
+from markdown_to_html import generate_page
 
 def main():
   copy_static_to_public()
+  generate_page("content/index.md", "template.html", "public/index.html")
 
 main()
