8a61ef5fef2470be14ddc85cdcc6d9a5f2a884c0
Demilade Omitiran <omitirand@gmail.com>
2024-04-26 19:59:04 +0100

    Use generate_pages_recursive function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from copy_static_to_public import copy_static_to_public
-from markdown_to_html import generate_page
+from markdown_to_html import generate_pages_recursive
 
 def main():
   copy_static_to_public()
-  generate_page("content/index.md", "template.html", "public/index.html")
+  generate_pages_recursive("content", "template.html", "public")
 
 main()
